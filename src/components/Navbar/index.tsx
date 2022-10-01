02f879f03cad9a2ece04b2095601b9608a257405
Leandro <llopesnunes@gmail.com>
2022-09-30 21:34:26 -0300

    Navbar styling

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,28 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
 const Navbar = () => {
   return (
-    <nav className="btg-primary">
-      <div>
-        <a href="link">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
           <h4>Carros Top</h4>
         </a>
-        <div>
-          <ul>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#carros-top-navbar"
+          aria-controls="carros-top-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id='carros-top-navbar'>
+          <ul className="navbar-nav offset-md-2 main-menu">
             <li>
               <a href="link">Home</a>
             </li>

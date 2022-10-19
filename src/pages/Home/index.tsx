d95bdb0886e3607771b1fe56ee21212ad5fe4096
Leandro <llopesnunes@gmail.com>
2022-10-19 20:52:59 -0300

    Finalizando estilização da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as MainImage } from 'assets/images/car-header 1.svg';
 import Navbar from 'components/Navbar';
 import './styles.css';
+import ButtonIcon from '../../components/Navbar/ButtonIcon/index';
 
 const Home = () => {
   return (
@@ -10,12 +11,16 @@ const Home = () => {
         <div className="home-card">
           <div className="home-image-container">
             <MainImage />
-                      <h2>O carro perfeito para você</h2>
-                      <h6>Conheça nossos carros e dê mais um passo na realização do seu sonho</h6>
+            <h2>O carro perfeito para você</h2>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
           </div>
 
           <div className="home-content-container">
-            <h1>Comece a navegar agora</h1>
+            <ButtonIcon />
+            <p>Comece agora a navegar</p>
           </div>
         </div>
       </div>
